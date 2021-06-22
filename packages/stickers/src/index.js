bd70e0dd3412cf7dbd6a4042a4477371336b504f
Divyaraj Masani <divyaraj.masani@rtcamp.com>
2021-06-22 17:58:23 +0530

    * Adds stickers for Batch 04, story template: How to get Safari’s privacy features in Chrome.

--- packages/stickers/src/index.js
+++ packages/stickers/src/index.js
@@ -39,6 +39,7 @@ import * as nycPartyRoundUpStickers from './nyc-party-round-up';
 import * as aDayInTheLifeStickers from './a-day-in-the-life';
 import * as orientExpressStickers from './venice-simplon-orient-express';
 import * as creativeGuideToTravelStickers from './creative-guide-to-travel';
+import * as getSafarisPrivacyFeaturesStickers from './get-safaris-privacy-features';
 
 export default {
   ...beautyStickers,
@@ -64,4 +65,5 @@ export default {
   ...aDayInTheLifeStickers,
   ...orientExpressStickers,
   ...creativeGuideToTravelStickers,
+  ...getSafarisPrivacyFeaturesStickers,
 };
